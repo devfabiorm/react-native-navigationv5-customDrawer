9cbd21540472cdfb045689e7fb32ef71da83ebc0
Fabio Ribeiro Martins <5039966-fabiormdkp@users.noreply.gitlab.com>
2020-05-06 12:47:29 -0300

    Initial Commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const HomeScreen = () => {
+
+    return(
+        <Text>Home Screen</Text>
+    );
+}
+
+export default HomeScreen;

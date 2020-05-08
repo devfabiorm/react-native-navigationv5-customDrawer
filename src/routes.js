6aa800e73d03d494bf9792642bfa3a9cde6caf3c
Fabio Ribeiro Martins <5039966-fabiormdkp@users.noreply.gitlab.com>
2020-05-07 23:55:19 -0300

    Adding Icons to the side bar menu

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import { Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import CustomDrawerContent from './components/CustomDrawerContent';
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
@@ -23,8 +25,16 @@ const Routes = () => {
                 drawerContentOptions={{activeTintColor: 'orange'}}
                 drawerContent={props => <CustomDrawerContent {...props} />} 
             >
-                <Drawer.Screen name="Home" component={HomeScreen} />
-                <Drawer.Screen name="Settings" component={SettingsScreen} />
+                <Drawer.Screen name="Home" component={HomeScreen} options={{
+                    drawerIcon: ({focused, color, size}) => <Icon name="home" size={size} color={color} />
+                }} />
+                <Drawer.Screen
+                name="Settings"
+                component={SettingsScreen}
+                options={{
+                drawerIcon: ({focused, color, size}) => <Icon name="settings" size={size} color={color} />
+                }}
+                />
             </Drawer.Navigator>
         );
     }
